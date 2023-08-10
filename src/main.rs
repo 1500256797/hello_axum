a72edfabd8cf7c077336696ba012c7966afbb144
ouhuang <1500256797@qq.com>
2023-08-10 16:14:57 +0800

    127.0.0.1 to 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -138,7 +138,7 @@ async fn main() {
             .layer(cors)
         );
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::info!("listening on {}", addr);
     axum_server::bind(addr)
         .serve(app.into_make_service())
