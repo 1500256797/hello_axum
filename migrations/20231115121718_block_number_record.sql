671d4d5d8064d72cd1b07dd05e9c60a434fce46f
ouhuang <1500256797@qq.com>
2023-11-16 14:44:52 +0800

    更新sql

--- migrations/20231115121718_block_number_record.sql
+++ migrations/20231115121718_block_number_record.sql
@@ -1,14 +1,11 @@
--- Create extension for case-insensitive citext type
-CREATE EXTENSION IF NOT EXISTS citext;
-
 -- Table structure for BlockNumberRecord
 CREATE TABLE IF NOT EXISTS "block_number_record" (
   "id" serial PRIMARY KEY,
-  "chain_type" citext NOT NULL DEFAULT 'bsc',
+  "chain_type" varchar(191) NOT NULL DEFAULT 'bsc',
   "wss_url" varchar(191) NOT NULL DEFAULT '',
   "https_url" varchar(191) NOT NULL DEFAULT '',
   "contract_addr" varchar(191) NOT NULL DEFAULT '',
-  "event_type" citext NOT NULL DEFAULT 'JoinActivity',
+  "event_type" varchar(191) NOT NULL DEFAULT 'JoinActivity',
   "event_signature" varchar(191) NOT NULL DEFAULT '',
   "from_block" integer NOT NULL DEFAULT '31244297',
   "last_block" integer NOT NULL,
--- migrations/20231115132032_change_block_number_field_type.sql
+++
@@ -1,5 +0,0 @@
--- Add migration script here
--- Alter the table to change data type from citext to varchar
-ALTER TABLE "block_number_record"
-  ALTER COLUMN "chain_type" TYPE varchar(191),
-  ALTER COLUMN "event_type" TYPE varchar(191);
